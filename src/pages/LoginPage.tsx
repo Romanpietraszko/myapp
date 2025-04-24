f45d53ef1f87301460878d339fd2d6f483f0a1c0
Roman <roman.pietraszko@onet.eu>
2025-04-24 10:29:37 +0200

    Utworzono strone logowania z logiem czyli LoginPage.tsx

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,27 @@
+// src/pages/LoginPage.tsx
+import { useNavigate } from 'react-router-dom'; // React Router
+import log from '../log.png'; // Zaimportuj plik logo
+import './LoginPage.css'; // Stylowanie
+
+const LoginPage = () => {
+  const navigate = useNavigate(); // Hook do nawigacji
+
+  const handleLogoClick = () => {
+    navigate('/home'); // Przekierowanie do głównej zawartości
+  };
+
+  return (
+    <div className="login-container">
+      {/* Logo */}
+      <div className="logo-container" onClick={handleLogoClick}>
+        <img 
+          src={log} // Użyj zaimportowanego pliku
+          alt="Logo Promolean" 
+          className="logo-image" // Dodajemy klasę CSS dla lepszego kontrolowania wyglądu
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
